c3381f2671c4c8294045d1d5f0eaf938c80b97f6
Ángel Tapia <41277597+angel-tapia@users.noreply.github.com>
2020-08-18 17:50:29 -0500

    Update Class_Breakout.py

--- Game/Class_Breakout.py
+++ Game/Class_Breakout.py
@@ -2,28 +2,50 @@ import pygame
 import random
 from Constants import *
 
-class Ball:
+class Ball(pygame.sprite.Sprite):
 
-    def __init__ (self):
-        self.x = screenWidth//2
-        self.y = screenHeight//2
-        
+    def __init__ (self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((widthBall, heightBall))
+        self.image.fill((255, 255, 255))
+        self.image.set_colorkey((0, 0, 0))
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.directionX = -1
+        self.directionY = -1
 
+    def flipDirectionX (self):
+        self.directionX *= -1
+
+    def flipDirectionY (self):
+        self.directionY *= -1
+    
     def move(self):
-        return False
+        self.rect.x += speedBall * self.directionX
+        self.rect.y += speedBall * self.directionY
 
     def valid(self):
-        return False
+        if self.rect.x <= 0 or self.rect.x >= screenWidth-widthBall:
+            self.flipDirectionX()
+        if self.rect.y <= 0:
+            self.flipDirectionY()
+        if self.rect.y > screenHeight:
+            return False
+        else :
+            return True
 
-    def setDirection(self, direction):
-        return False
 
 class Brick(pygame.sprite.Sprite):
 
     def __init__ (self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((widthBrick,heightBrick))
+        self.image = pygame.Surface((widthBrick, heightBrick))
         self.image.fill((200, 100, 200))
+        self.image.set_colorkey((0, 0, 0))
+        self.mask = pygame.mask.from_surface(self.image)
+        
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -35,6 +57,8 @@ class Paddle(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((widthPaddle, heightPaddle))
         self.image.fill((146, 168, 209))
+        self.image.set_colorkey((0, 0, 0))
+        self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect.center = (screenWidth//2, screenHeight-100)
 
