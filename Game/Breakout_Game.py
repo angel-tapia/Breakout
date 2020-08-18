a770adae4c79c25254562fb0448231eb484b263f
Ángel Tapia <41277597+angel-tapia@users.noreply.github.com>
2020-08-18 11:16:35 -0500

    Update Breakout_Game.py

--- Game/Breakout_Game.py
+++ Game/Breakout_Game.py
@@ -10,12 +10,18 @@ class Game:
         self.running = True
         self.end = True
         self.pause = False
-        self.screen = pygame.display.set_mode((screenLarge, screenWidth))
+        self.screen = pygame.display.set_mode((screenWidth, screenHeight))
         self.points = 0
         self.score = 0
         self.ball = Ball()
         self.paddle = Paddle()
-
+        self.Paddle_list = pygame.sprite.Group()
+        self.Paddle_list.add(self.paddle)
+        self.Brick_list = pygame.sprite.Group()
+        for i in range (0,10):
+            for j in range (0,4):
+                self.brick = Brick(40 + i*70, 15 + j*35)
+                self.Brick_list.add(self.brick)
 
     def simulate(self):
         pygame.time.delay(80)
@@ -35,6 +41,8 @@ class Game:
 
         self.paddle.direction = parsingKey(self.key)
         self.paddle.move()
+        self.Paddle_list.update()
+        self.Brick_list.update()
 
     def draw(self):
         #Display points obtained actually
@@ -42,7 +50,9 @@ class Game:
         pygame.display.set_caption("Points = " + str(self.points))
         
         #Draw
-        pygame.draw.rect(self.screen,(146, 168, 209),(self.paddle.x, self.paddle.y , largePaddle, widthPaddle))
+        #pygame.draw.rect(self.screen,(146, 168, 209),(self.paddle.x, self.paddle.y , widthPaddle, heightPaddle))
+        self.Paddle_list.draw(self.screen)
+        self.Brick_list.draw(self.screen)
         pygame.display.update()
 
     def over(self):
@@ -51,8 +61,8 @@ class Game:
                 self.end = False
                 return
         font = pygame.font.Font('freesansbold.ttf', 32)
-        text = font.render('Game over', True, (255,0,0), (0,0,0))
+        text = font.render('Game over', True, (255, 0, 0), (0, 0, 0))
         textObj = text.get_rect()
-        textObj.center = (screenLarge// 2, screenWidth // 2)
+        textObj.center = (screenWidth// 2, screenHeight // 2)
         self.screen.blit(text, textObj)
         pygame.display.update()
